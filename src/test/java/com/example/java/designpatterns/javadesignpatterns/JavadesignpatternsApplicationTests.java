cbe61753b722248887e12a53f854100f4aa06b68
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2020-12-02 19:44:03 +0530

    initial commit

---
+++ src/test/java/com/example/java/designpatterns/javadesignpatterns/JavadesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.designpatterns.javadesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
